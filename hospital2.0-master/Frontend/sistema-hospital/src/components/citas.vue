3dc1f5a7d06163a464853aeedd87ec061e00cc56
JosGom <110825258+Husbam@users.noreply.github.com>
2024-06-13 14:52:49 -0600

    Add files via upload
    Agregar los archivos con el avance de clase.

---
+++ hospital2.0-master/Frontend/sistema-hospital/src/components/citas.vue
@@ -0,0 +1,70 @@
+<template>
+    <div class="flex flex-col md:flex-row justify-center items-center">
+        <div class="w-max md:w-2/4 p-8">
+            <form action="">
+                <h1 class="text-2xl font-bold text-gray-800 dark:text-white mb-4">Citas:</h1>
+
+                        <!--  -->
+                <div class="mb-6">
+                    <div class="mt-4 py-2">
+                        <div>
+                            <label for="id_busqueda" class="block text-gray-700 dark:text-white mb-1">Buscar ID</label>
+                            <input type="text" id="id_busqueda" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" placeholder="Introduce el ID a buscar" required>
+                        </div>
+                    </div>
+                    <div class="grid grid-cols-2 gap-4">
+                        <div>
+                            <label for="nombre" class="block text-gray-700 dark:text-white mb-1">Nombre</label>
+                            <input type="text" id="nombre" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" required>
+                        </div>
+                        <div>
+                            <label for="apellido" class="block text-gray-700 dark:text-white mb-1">Apellidos</label>
+                            <input type="text" id="apellido" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" required>
+                        </div>
+                        <div>
+                            <label for="fechaNacimiento" class="block text-gray-700 dark:text-white mb-1">Fecha de Nacimiento</label>
+                            <input type="date" id="fechaNacimiento" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" required>
+                        </div>
+                        <div>
+                            <label for="genero" class="block text-gray-700 dark:text-white mb-1">Género</label>
+                            <select id="genero" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
+                                <option selected>Selecciona tu género</option>
+                                <option value="Masculino">Masculino</option>
+                                <option value="Femenino">Femenino</option>
+                                <option value="Otro">Otro</option>
+                            </select>
+                        </div>
+                        <div>
+                            <label for="fechaCita" class="block text-gray-700 dark:text-white mb-1">Fecha de la cita</label>
+                            <input type="date" id="fechaCita" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" required>
+                        </div>
+                        <div>
+                            <label for="horaCita" class="block text-gray-700 dark:text-white mb-1">Hora de la cita</label>
+                            <input type="time" id="horaCita" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" required>
+                        </div>
+                        <div>
+                            <label for="numeroTelefono" class="block text-gray-700 dark:text-white mb-1">Número de Teléfono</label>
+                            <input type="tel" id="numeroTelefono" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" required>
+                        </div>
+                        <div>
+                            <label for="email" class="block text-gray-700 dark:text-white mb-1">Correo Electronico</label>
+                            <input type="email" id="email" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" required>
+                        </div>
+                        <div>
+                        </div>
+                    </div>
+                    <div class="mt-4">
+                        <label for="motivo" class="block text-gray-700 dark:text-white mb-1">Motivo de la cita</label>
+                        <textarea id="motivo" class="w-full rounded-lg border py-2 px-3 dark:bg-gray-700 dark:text-white dark:border-none" required></textarea>
+                    </div>
+                    
+                        <div class="mt-8 flex justify-start">
+                            <button class="bg-teal-500 text-white px-4 py-2 rounded-lg hover:bg-teal-700 dark:bg-teal-600 dark:text-white dark:hover:bg-teal-900">Agendar Cita</button>
+                        </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
+
+</template>
---
+++ hospital2.0-master/Frontend/sistema-hospital/src/components/listarCitas.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="shadow-lg rounded-lg overflow-hidden mx-4 md:mx-10 mt-8">
+        <table class="w-full table-fixed">
+          <thead>
+            <tr class="bg-gray-100">
+              <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Nombre</th>
+              <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Fecha</th>
+              <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Peso</th>
+              <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Talla</th>
+              <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Edad</th>
+              <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">P.A</th>
+              <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Diagnóstico</th>
+              <th class="w-1/4 py-4 px-6 text-left text-gray-600 font-bold uppercase">Prescripción Médica</th>
+            </tr>
+          </thead>
+          <tbody class="bg-white">
+            <tr>
+              <td class="py-4 px-6 border-b border-gray-200">John Doe</td>
+              <td class="py-4 px-6 border-b border-gray-200 truncate">2024-06-10</td>
+              <td class="py-4 px-6 border-b border-gray-200">75 kg</td>
+              <td class="py-4 px-6 border-b border-gray-200">180 cm</td>
+              <td class="py-4 px-6 border-b border-gray-200">35 años</td>
+              <td class="py-4 px-6 border-b border-gray-200">120/80 mmHg</td>
+              <td class="py-4 px-6 border-b border-gray-200">Hipertensión</td>
+              <td class="py-4 px-6 border-b border-gray-200">Recetar medicamento X</td>
+            </tr>
+            <!-- Más filas de datos aquí -->
+            <tr>
+            <td class="py-4 px-6 border-b border-gray-200">John Smith</td>
+            <td class="py-4 px-6 border-b border-gray-200">2024-06-12</td>
+            <td class="py-4 px-6 border-b border-gray-200">80 kg</td>
+            <td class="py-4 px-6 border-b border-gray-200">175 cm</td>
+            <td class="py-4 px-6 border-b border-gray-200">45 años</td>
+            <td class="py-4 px-6 border-b border-gray-200">140/95 mmHg</td>
+            <td class="py-4 px-6 border-b border-gray-200">Obesidad</td>
+            <td class="py-4 px-6 border-b border-gray-200">Recetar medicamento Z</td>
+            </tr>
+            <tr>
+                <td class="py-4 px-6 border-b border-gray-200">Alice Johnson</td>
+                <td class="py-4 px-6 border-b border-gray-200">2024-06-13</td>
+                <td class="py-4 px-6 border-b border-gray-200">65 kg</td>
+                <td class="py-4 px-6 border-b border-gray-200">170 cm</td>
+                <td class="py-4 px-6 border-b border-gray-200">30 años</td>
+                <td class="py-4 px-6 border-b border-gray-200">115/75 mmHg</td>
+                <td class="py-4 px-6 border-b border-gray-200">Ninguno</td>
+                <td class="py-4 px-6 border-b border-gray-200">Sin prescripción</td>
+            </tr>
+            <tr>
+                <td class="py-4 px-6 border-b border-gray-200">Michael Brown</td>
+                <td class="py-4 px-6 border-b border-gray-200">2024-06-14</td>
+                <td class="py-4 px-6 border-b border-gray-200">70 kg</td>
+                <td class="py-4 px-6 border-b border-gray-200">185 cm</td>
+                <td class="py-4 px-6 border-b border-gray-200">50 años</td>
+                <td class="py-4 px-6 border-b border-gray-200">125/85 mmHg</td>
+                <td class="py-4 px-6 border-b border-gray-200">Colesterol alto</td>
+                <td class="py-4 px-6 border-b border-gray-200">Recetar medicamento A</td>
+            </tr>
+            <tr>
+                <td class="py-4 px-6 border-b border-gray-200">Sophia Garcia</td>
+                <td class="py-4 px-6 border-b border-gray-200">2024-06-15</td>
+                <td class="py-4 px-6 border-b border-gray-200">60 kg</td>
+                <td class="py-4 px-6 border-b border-gray-200">160 cm</td>
+                <td class="py-4 px-6 border-b border-gray-200">25 años</td>
+                <td class="py-4 px-6 border-b border-gray-200">110/70 mmHg</td>
+                <td class="py-4 px-6 border-b border-gray-200">Ninguno</td>
+                <td class="py-4 px-6 border-b border-gray-200">Sin prescripción</td>
+            </tr>
+          </tbody>
+        </table>
+
+    </div>
+</template>
